397fb6befa2878fe53a3a5a6606dd83e8f67a671
interoph <interoph@users.noreply.github.com>
2019-12-28 12:56:54 -0800

    connected netlify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Test
+          Learn React - Continuous Deployment Test using Netlify and GitHub
         </a>
       </header>
     </div>
